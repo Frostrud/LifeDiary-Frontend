263fc2ad2a56415248af6dae5c7d01bf77986673
Frostrud <hofossfrostrud@gmail.com>
2023-05-25 21:49:32 +0200

    Fixed some html for the login.

--- src/login.js
+++ src/login.js
@@ -2,14 +2,21 @@ import React from "react";
 import Middiv from "./Middiv";
 
 const Login = () => {
-    return (
-        <div>
-          <Middiv>
-            <h1>Login</h1>
-          </Middiv>
-        </div>
-          
-    );
+  return (
+    <Middiv>
+      <div className="loginDiv">
+      <h2>Login</h2>
+      <form>
+        <label for="email">Email:</label>
+        <input type="text" id="email" name="email" required></input><br></br>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required></input><br></br>
+        <input type="submit" value="Login"></input><br></br>
+      </form>
+    </div>
+    </Middiv>
+    
+  );
 }
 
 export default Login;
